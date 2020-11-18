ed0e03bdee33cc6c06698016b357ce9bc3bf5a08
nadine <nadinetruter02@gmail.com>
2020-11-18 15:22:55 -0800

    added search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from profiles_api import serializers
+from rest_framework import filters
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -103,3 +104,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
